2d2e7dcdbba247c051d292a2a3ee9b1bcbb47ee3
Ahmedijaz11 <101427579+Ahmedijaz11@users.noreply.github.com>
2022-05-19 08:04:00 +0500

    Selected All The Required Elements

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+//Selecting Elements
+
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.querySelector('#score--1');
+const diceEL = document.querySelector('.dice');
+const btnroll = document.querySelector('.btn--roll')
+const btnnew = document.querySelector('.btn--new')
+const btnhold = document.querySelector('.btn--hold')
+
+//starting Conditions
+score0EL.textContent= 0;
+score1EL.textContent= 0;
+diceEL.classList.add('hidden');
+
+btnroll.addEventListener('click', function () {
+
+    // 1. Genrating A Random Dice Roll
+
+
+    // 2. Display Dice
+
+
+    // 3. Check for rolled, if 1, switch to next Player
+
+
+
+    });
