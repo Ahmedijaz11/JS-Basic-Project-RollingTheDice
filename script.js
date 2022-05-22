745685231ba4d6f0a671f1c0576ab3c546c76ef2
Ahmedijaz11 <101427579+Ahmedijaz11@users.noreply.github.com>
2022-05-22 13:25:20 +0500

    game working done

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 'use strict';
 
+//switch player function
+
+const switchplayer = function ()
+{
+
+     // switch the player
+     document.getElementById(`current--${activePlayer}`).textContent= 0;
+     currentScore = 0;
+     activePlayer = activePlayer === 0 ? 1 : 0;
+
+     player0EL.classList.toggle('player--active');
+     player1EL.classList.toggle('player--active');
+     
+}
+
 //Selecting Elements
 const player0EL = document.querySelector('.player--0');
 const player1EL = document.querySelector('.player--1');
@@ -24,8 +39,11 @@ diceEL.classList.add('hidden');
 const scores = [0 , 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 btnroll.addEventListener('click', function () {
+
+    if(playing){
 
     // 1. Genrating A Random Dice Roll
 
@@ -52,17 +70,58 @@ btnroll.addEventListener('click', function () {
 
     
     else {
-        // switch the player
-        document.getElementById(`current--${activePlayer}`).textContent= 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
 
-        player0EL.classList.toggle('player--active');
-        player1EL.classList.toggle('player--active');
+        switchplayer();
+        // switch the player
+        // document.getElementById(`current--${activePlayer}`).textContent= 0;
+        // currentScore = 0;
+        // activePlayer = activePlayer === 0 ? 1 : 0;
+
+        // player0EL.classList.toggle('player--active');
+        // player1EL.classList.toggle('player--active');
         
 
     }
 
+ }
+
+ });
+
+    //Hold Button Functionality
+
+    btnhold.addEventListener('click', function() {
+
+    if(playing){
+
+        //Add score to Active Player
+        scores[activePlayer]+= currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent= scores[activePlayer];
 
 
-    });
+        //if player score is > 100
+        //finish the game
+        if(scores[activePlayer] >= 20) {
+
+            playing = false;
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+            
+            
+
+
+
+        }
+        
+          else{
+        //else switch to next player
+        switchplayer();
+          }
+
+    }
+});
+
+
+btnnew.addEventListener('click', function(){
+
+    document.location.reload(true);
+})
